7f70cf819d135ce114d20a85c3874e4e3c40ec1e
XiaoLFeng <gm@x-lf.cn>
2023-12-26 17:06:38 +0800

    补丁

--- api/request/tokenApi.go
+++ api/request/tokenApi.go
@@ -3,11 +3,11 @@ package request
 import "github.com/gogf/gf/v2/frame/g"
 
 type TokenCreateReq struct {
-	g.Meta `path:"/create" tags:"创建" method:"get" summary:"创建 TokenServiceImpl"`
+	g.Meta `path:"/create" tags:"创建" method:"get" summary:"创建 Token"`
 }
 type TokenCreateRes struct{}
 
 type TokenVerifyReq struct {
-	g.Meta `path:"/verify" tags:"验证" method:"get" summary:"验证 TokenServiceImpl"`
+	g.Meta `path:"/verify" tags:"验证" method:"get" summary:"验证 Token"`
 }
 type TokenVerifyRes struct{}
